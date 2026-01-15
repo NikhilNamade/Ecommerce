247116e39054b25500a9f8eb5a37e226d15d010d
NikhilNamade <nikhilnamade39@gmail.com>
2026-01-15 22:00:56 +0530

    Update CORS configuration for frontend URL

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend-url.onrender.com"
+      "https://ecommerce-gray-five-88.vercel.app/"
     ],
     credentials: true,
   })
