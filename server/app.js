666552aa5128e2f1b3fd9d42f7a62e87646115ed
NikhilNamade <nikhilnamade39@gmail.com>
2026-01-17 13:54:07 +0530

    Simplify CORS configuration to allow all origins

--- server/app.js
+++ server/app.js
@@ -21,14 +21,7 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin) return callback(null, true);
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "*",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
